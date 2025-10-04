e70c19f940e6b34b6c0924c8eb8f214867bc3c53
Danesp0409 <danielsantiago12543@gmail.com>
2025-10-03 22:17:09 -0500

    Update RabbitConfig.java

--- src/main/java/com/example/demo/config/RabbitConfig.java
+++ src/main/java/com/example/demo/config/RabbitConfig.java
@@ -6,28 +6,28 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-// Declara la clase como un componente de servicio de Spring
+//Esta anotación le dice a Spring que esta clase contiene definiciones de beans que deben ser gestionadas por el contenedor de Spring.//
 @Configuration
 public class RabbitConfig {
-
+//se definen los nombres de las colas como constantes
     public static final String ORDERS_QUEUE = "ordersQueue";
     public static final String RESULTS_QUEUE = "resultsQueue";
 
     @Bean
     public Queue ordersQueue() {
         return new Queue(ORDERS_QUEUE, false);
-    }
+    }//los mensajes en esta cola se perderán si el servidor de RabbitMQ se reinicia.
 
     @Bean
     public Queue resultsQueue() {
         return new Queue(RESULTS_QUEUE, true);
-    }
+    }//los mensajes persistan en el disco y no se pierdan si el servidor se cae o se reinicia.
 
     // Convertidor JSON
     @Bean
     public Jackson2JsonMessageConverter messageConverter() {
         return new Jackson2JsonMessageConverter();
-    }
+    }//Convierte objetos de Java a formato JSON para ser enviados a la cola y viceversa
 
     @Bean
     public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
